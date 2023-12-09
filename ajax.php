2861b00601a0998e3304f4e72a5e7a8fa473b6c0
Henry Ibrahima Vincent COLY <vincoly7@gmail.com>
2023-12-09 08:52:14 +0000

    afficher la liste des contacts

--- ajax.php
+++ ajax.php
@@ -1,1 +1,37 @@
 <?php
+
+require './contact.class.php';
+
+
+if (!empty($_POST['action']) and $_POST['action'] == 'fetch_contacts') {
+     $contactInstance = new Contact();
+     $contacts = $contactInstance->get_contacts();
+     $result = "";
+
+     if (count($contacts) > 0) {
+          foreach($contacts as $contact) {
+               $result .= "
+                    <tr>
+                         <td>{$contact['categorie']} </td>
+                         <td>{$contact['prenom']}</td>
+                         <td>{$contact['categorie']}</td>
+                         <td><button type='button' value='{$contact['id']}'>Voir</button></td>
+                    </tr>
+               ";
+          }
+     } else {
+          $result .= "
+               <tr>
+                    <td align='center'>Data not Found</td>
+               </tr>
+          ";
+     }
+
+     $res = [
+          'data' => $result
+     ];
+
+     echo json_encode($res);
+
+     return false;
+}
